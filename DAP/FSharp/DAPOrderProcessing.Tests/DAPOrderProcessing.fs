ce7e873b271dfb8b8914412946bb71e5e79ff1c9
tjaskula <thomasv1000@hotmail.fr>
2014-10-08 00:11:35 +0200

    Added first unit test (adding an order item to an empty order)

--- DAP/FSharp/DAPOrderProcessing.Tests/DAPOrderProcessing.fs
+++ DAP/FSharp/DAPOrderProcessing.Tests/DAPOrderProcessing.fs
@@ -1,4 +1,15 @@
 ﻿namespace DAPOrderProcessing.Tests
 
-type Class1() = 
-    member this.X = "F#"
+open DAPOrderProcessing
+open DAPOrderProcessing.OrderHandling
+
+open Xunit
+open FsUnit.Xunit
+
+module ``DAP order processing tests`` =
+
+    [<Fact>]
+    let ``Should add order item to empty order``() =
+        let orderItem = OrderItem "item1"
+        let order = addToEmptyState orderItem
+        order |> should equal (PaymentExpected {UnpaidItems = [orderItem]})
