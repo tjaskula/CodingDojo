f306db1c96dc72dae2d97315c64529234421968a
tjaskula <thomasv1000@hotmail.fr>
2014-10-07 23:50:15 +0200

    Added types and functions to the main file

--- DAP/FSharp/DAPOrderProcessing/DAPOrderProcessing.fs
+++ DAP/FSharp/DAPOrderProcessing/DAPOrderProcessing.fs
@@ -1,3 +1,26 @@
 ﻿namespace DAPOrderProcessing
 
 type OrderItem = OrderItem of string
+type PaymentReceipt = {OrderRef : string; PayedAmount : decimal}
+
+type EmptyState = NoItems // allows to handle explicitly this state.
+type PaymentExpectedState = {UnpaidItems : OrderItem list}
+type PayedState = {PayedItems : OrderItem list; PayedAmount : decimal}
+type CancelledState = NoAction
+type CompletedState = {Payed : PayedState; Received : PaymentReceipt}
+
+type Order =
+    | Empty of EmptyState
+    | PaymentExpected of PaymentExpectedState
+    | Payed of PayedState
+    | Cancelled of CancelledState
+
+
+module OrderHandling =
+
+    let addToEmptyState orderItem =
+        Order.PaymentExpected {UnpaidItems = [orderItem]}
+
+    let addToPaymentExpectedState state orderItemToAdd =
+        let newList = orderItemToAdd :: state.UnpaidItems
+        Order.PaymentExpected {state with UnpaidItems = newList}
